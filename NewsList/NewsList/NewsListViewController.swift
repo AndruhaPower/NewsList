e48cba244111478c4d337376a6ec8e0ec27111b5
AndruhaPower <pazit@yandex.ru>
2020-01-14 00:08:42 +0300

    + класс главного контроллера новостей

--- NewsList/NewsList/NewsListViewController.swift
+++ NewsList/NewsList/NewsListViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class NewsListViewController: UIViewController {
+    
+    var feedTableView = NewsListTableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupView()
+         self.view.backgroundColor = .white
+         self.navigationController?.navigationBar.prefersLargeTitles = true
+         self.title = "Новости"
         // Do any additional setup after loading the view.
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        self.setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.view = self.feedTableView
     }
 
 
