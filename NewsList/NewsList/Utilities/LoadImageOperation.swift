f08fc5d12b062f4ed44bf5131bde3d05db0eb6ee
AndruhaPower <pazit@yandex.ru>
2020-01-14 01:15:58 +0300

    + помощник в зарузке картинок в параллельном потоке

--- NewsList/NewsList/Utilities/LoadImageOperation.swift
+++ NewsList/NewsList/Utilities/LoadImageOperation.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Andrew. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class LoadImageOperation: Operation {
+    
+    var url: URL?
+    var completion: ((UIImage) -> ())?
+    override func main() {
+        if let data = try? Data(contentsOf: self.url ?? URL(string: "https://vk.com/images/dquestion_v.png")!),
+            let image = UIImage(data: data) {
+            DispatchQueue.main.async { self.completion?(image) }
+        }
+    }
+}
