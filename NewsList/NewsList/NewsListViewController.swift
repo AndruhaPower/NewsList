67654e844cbfcb5fa8e6fe46a6e917a298766ceb
AndruhaPower <pazit@yandex.ru>
2020-01-13 19:12:44 +0300

    initial commit

---
+++ NewsList/NewsList/NewsListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListViewController.swift
+//  NewsList
+//
+//  Created by Andrew on 13/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
