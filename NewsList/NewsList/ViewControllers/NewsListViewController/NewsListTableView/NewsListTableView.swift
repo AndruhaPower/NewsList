8e02afb81624c8823f56b8ea9ed6d3c71ec72e69
AndruhaPower <pazit@yandex.ru>
2020-01-16 21:21:38 +0300

    реализован бесконечный скроллинг путем пагинации и подгрузки  по 20 новостей в пачке реализована подгрузка с помощью refreshControl

--- NewsList/NewsList/ViewControllers/NewsListViewController/NewsListTableView/NewsListTableView.swift
+++ NewsList/NewsList/ViewControllers/NewsListViewController/NewsListTableView/NewsListTableView.swift
@@ -17,25 +17,34 @@ class NewsListTableView: UITableView {
     
     public weak var tableViewPushDelegate: TableViewPushDelegate?
     
+    var page: Int = 1
     var news: [PostModel] = []
+    
     let operationQueue = OperationQueue()
     var cachedImages: [IndexPath : UIImage] = [:]
+    var newsServices = NewsServices()
+    
+    var isLoading: Bool = false
     
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
         self.setupTableView()
+        self.configRefreshControl()
+        self.getNews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+// MARK: setting up TableView
     
     private func setupTableView() {
         self.backgroundColor = .white
         self.delegate = self
         self.dataSource = self
+        self.prefetchDataSource = self
         self.register(NewsListTableViewCell.self, forCellReuseIdentifier: NewsListTableViewCell.reuseId)
-
     }
     
     private func setPicture(for cell: NewsListTableViewCell, at indexPath: IndexPath) {
@@ -43,7 +52,7 @@ class NewsListTableView: UITableView {
         guard self.cachedImages[indexPath] == nil else {
             return cell.postImage.image = cachedImages[indexPath]
         }
-        
+    
         let operation = LoadImageOperation()
         operation.url = URL(string: self.news[indexPath.row].postImageURL)
         self.operationQueue.addOperation(operation)
@@ -54,7 +63,39 @@ class NewsListTableView: UITableView {
             } else { cell.frame = .zero }
         }
     }
+    
+    private func getNews() {
+        self.newsServices.getNews(page: self.page) { [weak self] (news) in
+            guard let self = self else { return }
+            self.news = news
+            self.reloadData()
+        }
+    }
+    
+    private func configRefreshControl() {
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.attributedTitle = NSAttributedString(string: "Обновление...")
+        self.refreshControl?.tintColor = .darkGray
+        self.refreshControl?.addTarget(self, action: #selector(refreshNews), for: .valueChanged)
+    }
+    
+    @objc func refreshNews(_ sender: Any) {
+        self.newsServices.getNews(page: 1) { (newPosts) in
+            guard let lastDownloaded = newPosts.last
+                , let firstDownloaded = newPosts.first
+                , let lastShowing = self.news.first
+                , firstDownloaded != lastShowing
+                , lastDownloaded > lastShowing
+                else { self.refreshControl?.endRefreshing(); return }
+            self.news = newPosts + self.news
+            let indexSet = IndexSet(integersIn: 0..<newPosts.count)
+            self.insertSections(indexSet, with: .automatic)
+            self.refreshControl?.endRefreshing()
+        }
+    }
 }
+
+// MARK: Extensions
 
 extension NewsListTableView: UITableViewDelegate, UITableViewDataSource {
     
@@ -85,5 +126,32 @@ extension NewsListTableView: UITableViewDelegate, UITableViewDataSource {
         self.setPicture(for: cell, at: indexPath)
         
         return cell
+    }
+}
+
+// MARK: EndlessScroll
+
+extension NewsListTableView: UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard let maxRow =  indexPaths.map({ $0.row }).max() else { return }
+        
+        if maxRow > self.news.count - 3,
+            !self.isLoading {
+            self.isLoading = true
+            self.page = Int(self.news.count / Constants.pageSize) + 1
+ 
+            self.newsServices.getNews(page: self.page) { [weak self] (olderPosts) in
+                guard let self = self
+                    , olderPosts.count != 0
+                    , let firstPost = olderPosts.first
+                    , let lastPost = self.news.last
+                    , lastPost > firstPost else { return }
+        
+                self.news.append(contentsOf: olderPosts)
+                self.reloadData()
+                self.isLoading = false
+            }
+        }
     }
 }
